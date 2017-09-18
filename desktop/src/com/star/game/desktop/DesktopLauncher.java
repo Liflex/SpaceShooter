6aae039184fdf2223d0d018df3922e386032c701
Liflex <Dmitartur@yandex.ru>
2017-09-19 00:26:57 +0300

    .

--- desktop/src/com/star/game/desktop/DesktopLauncher.java
+++ desktop/src/com/star/game/desktop/DesktopLauncher.java
@@ -9,8 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		float aspect = 480f / 854f;
-//        float aspect = 9f / 16f;
-//        float aspect = 3f / 4f;
+//      float aspect = 9f / 16f;
+//      float aspect = 3f / 4f;
 //		config.height = 900;
 //		config.width = (int)(config.height * aspect);
 
